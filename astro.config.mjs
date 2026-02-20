ccc96bf3de76a2aa34ae5cfc5d85387e4e0648af
James Conlon <conlonj25@gmail.com>
2026-02-20 23:58:17 +0100

    chore: acessibility, opengraph and sitemap improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://www.jamesconlon.dev/",
+  integrations: [icon(), sitemap()],
   output: "static",
   vite: {
     plugins: [tailwindcss()],
